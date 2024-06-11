ac8995f72afb0172b64acb56305acbb5cc25278a
kirankab7 <kirankab7@gmail.com>
2024-06-11 19:29:57 +0530

    Sample App

---
+++ app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.viewbinding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.textView.text = "Button clicked"
+        }
+    }
+}
